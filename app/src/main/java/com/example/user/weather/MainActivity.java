2283dd926c014a068848d9f2dfbde6a1b8e6241f
pritesh25 <priteshvishwakarma@gmail.com>
2018-11-21 16:06:03 +0530

    icon loaded into imageview 2 with optimization

--- app/src/main/java/com/example/user/weather/MainActivity.java
+++ app/src/main/java/com/example/user/weather/MainActivity.java
@@ -1,41 +1,23 @@
 package com.example.user.weather;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.example.user.weather.model.Weather;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
-import org.json.JSONException;
-
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private TextView cityText;
-    private TextView condDescr;
-    private TextView temp;
-    private TextView press;
-    private TextView windSpeed;
-    private TextView windDeg;
-
-    private TextView hum;
     private ImageView imgView;
 
     //https://openweathermap.org/weather-conditions
-    //1633
-    //1983
-
 
     String imageUrlHead      = "http://openweathermap.org/img/w/";
     String imageUrlBody      = "";
@@ -48,66 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
         imgView     = findViewById(R.id.condIcon);
         callIonWebService();
-        /*
-        String city = "London,UK";
 
-        cityText    = (TextView) findViewById(R.id.cityText);
-        condDescr   = (TextView) findViewById(R.id.condDescr);
-        temp        = (TextView) findViewById(R.id.temp);
-        hum         = (TextView) findViewById(R.id.hum);
-        press       = (TextView) findViewById(R.id.press);
-        windSpeed   = (TextView) findViewById(R.id.windSpeed);
-        windDeg     = (TextView) findViewById(R.id.windDeg);
-        imgView     = (ImageView) findViewById(R.id.condIcon);
-
-        JSONWeatherTask task = new JSONWeatherTask();
-        task.execute(new String[]{city});
-        */
     }
 
-    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
-
-        @Override
-        protected Weather doInBackground(String... params) {
-            Weather weather = new Weather();
-            String data = ((new WeatherHttpClient()).getWeatherData(params[0]));
-            Log.d(TAG,"data = "+params[0]);
-
-            try {
-                weather = JSONWeatherParser.getWeather(data);
-
-                // Let's retrieve the icon
-                weather.iconData = ((new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return weather;
-
-        }
-
-        @Override
-        protected void onPostExecute(Weather weather) {
-            super.onPostExecute(weather);
-
-            if (weather.iconData != null && weather.iconData.length > 0) {
-                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
-                imgView.setImageBitmap(img);
-            }
-
-            cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
-            condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
-            temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "�C");
-            hum.setText("" + weather.currentCondition.getHumidity() + "%");
-            press.setText("" + weather.currentCondition.getPressure() + " hPa");
-            windSpeed.setText("" + weather.wind.getSpeed() + " mps");
-            windDeg.setText("" + weather.wind.getDeg() + "�");
-
-        }
-    }
-
-    private void callIonWebService()
-    {
+    private void callIonWebService() {
         Ion.with(getApplicationContext())
                 .load("http://api.openweathermap.org/data/2.5/weather?q="+"Mumbai,In"+"&appid=82af4295b345889c281640e43c8d0862")
                 .asJsonObject()
